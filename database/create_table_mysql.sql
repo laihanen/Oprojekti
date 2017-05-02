ae9971a936b6241f69f2683878376d033da881fd
idaliinaleksandra <idaliinalaine@gmail.com>
2017-05-02 13:27:24 +0300

    tietokanta

---
+++ database/create_table_mysql.sql
@@ -0,0 +1,63 @@
+CREATE TABLE luoja (
+	id INT NOT NULL AUTO_INCREMENT,
+	nimi VARCHAR(100) NOT NULL,
+	sposti VARCHAR(100) NOT NULL,
+	salasana VARCHAR(20) NOT NULL,
+    PRIMARY KEY (id)
+) ENGINE=InnoDB;
+
+CREATE TABLE kysely (
+	id INT NOT NULL AUTO_INCREMENT,
+	nimi VARCHAR(100) NOT NULL,
+	luoja_id INT,
+    PRIMARY KEY (id),
+    FOREIGN KEY (luoja_id) REFERENCES luoja(id)
+) ENGINE=InnoDB;
+
+CREATE TABLE otsikko (
+    id INT NOT NULL AUTO_INCREMENT,
+    nimi VARCHAR(100),
+    PRIMARY KEY (id)
+) ENGINE=InnoDB;
+
+CREATE TABLE kysymystyyppi (
+    id INT NOT NULL AUTO_INCREMENT,
+    nimi varchar(100),
+    PRIMARY KEY (id)
+) ENGINE=InnoDB;
+
+CREATE TABLE kysymys (
+    id INT NOT NULL AUTO_INCREMENT,
+    kysymysteksti VARCHAR(200),
+    kysely_id INT,
+    otsikko_id INT,
+    kysymystyyppi_id INT,
+    PRIMARY KEY (id),
+    FOREIGN KEY (kysely_id) REFERENCES kysely(id),
+    FOREIGN KEY (otsikko_id) REFERENCES otsikko(id),
+    FOREIGN KEY (kysymystyyppi_id) REFERENCES kysymystyyppi(id)
+) ENGINE=InnoDB;
+
+CREATE TABLE vaihtoehto(
+    id INT NOT NULL AUTO_INCREMENT,
+    nimi VARCHAR(200),
+    kysymys_id INT,
+    PRIMARY KEY (id),
+    FOREIGN KEY (kysymys_id) REFERENCES kysymys(id)
+) ENGINE=InnoDB;
+
+CREATE TABLE vastaus(
+    id INT NOT NULL AUTO_INCREMENT,
+    avointekstivastaus VARCHAR(300),
+    kysymys_id INT,
+    PRIMARY KEY (id),
+    FOREIGN KEY (kysymys_id) REFERENCES kysymys(id)
+) ENGINE=InnoDB;
+
+CREATE TABLE vaihtoehtoVastaus(
+    vastaus_id INT,
+    vaihtoehto_id INT,
+    PRIMARY KEY (vastaus_id, vaihtoehto_id),
+    FOREIGN KEY (vastaus_id) REFERENCES vastaus(id),
+    FOREIGN KEY (vaihtoehto_id) REFERENCES vaihtoehto(id)
+) ENGINE=InnoDB;
---
+++ database/insert_test_data_mysql.sql
@@ -0,0 +1,29 @@
+INSERT INTO luoja (nimi, sposti, salasana)
+VALUES
+('Jorma Testaaja', 'jormatestaaja@joku.fi', 'salasana1234');
+
+INSERT INTO kysely (nimi)
+VALUES
+('Kyselyn lähtökohtana on tutkia Haaga-Helia ammattikorkeakoulun opiskelijoiden kouluviihtyvyyttä ja sen kehittämistä');
+
+INSERT INTO otsikko (nimi)
+VALUES
+('Perustiedot');
+
+INSERT INTO kysymystyyppi (nimi)
+VALUES
+('Yksi valinta');
+
+INSERT INTO kysymys (kysymysteksti)
+VALUES
+('Sukupuoli');
+
+INSERT INTO vaihtoehto(nimi)
+VALUES
+('N'),
+('M'),
+('Muu');
+
+INSERT INTO vastaus(avointekstivastuas)
+VALUES
+('');
