eefe54468eb94e4f7d6598ca3d7215336c75e831
laihanen <juho.laihanen@gmail.com>
2017-05-02 12:25:58 +0300

    first commit

---
+++ src/main/java/ohjelmisto/controller/KyselyController.java
@@ -0,0 +1,60 @@
+package ohjelmisto.controller;
+
+import ohjelmisto.ohjelmisto.dao.KyselyDAO;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.inject.Inject;
+
+/**
+ * Created by bferr on 2.5.2017.
+ */
+@Controller
+@RequestMapping(value="/secure")
+public class KyselyController {
+
+    @Inject
+    KyselyDAO dao;
+
+    public KyselyDAO getDao() {
+        return dao;
+    }
+
+    public void setDao(KyselyDAO dao) {
+        this.dao = dao;
+    }
+/*
+    @RequestMapping (value="id", method = RequestMethod.GET)
+    public String getCreateForm(Model model){
+        Kysely tyhjaKysely = new KyselyImpl();
+        tyhjaKysely.setNimi("oletusnimi");
+        model.addAttribute("kysely", tyhjaKysely);
+        return "secure/add";
+    }
+
+    @RequestMapping (value = "id", method = RequestMethod.GET)
+    public String create(@ModelAttribute(value="kysely") KyselyImpl kysely){
+        dao.lisaaKysely(kysely);
+        return "redirect:/kysely/" + kysely.getId();
+    }
+
+    @RequestMapping (value = "id", method = RequestMethod.GET)
+    public String getView(@PathVariable Integer id, Model model){
+        List<Kysely> kysely = dao.haeKysely();
+        model.addAttribute("kysely", kysely);
+        return "secure/view";
+    }
+    */
+
+    @RequestMapping ("kyselyt.json")
+    public @ResponseBody List<Kysely> haeKyselytJSON () {
+        List<Kysely> kyselyt = dao.haeKaikki();
+        return kyselyt;
+
+    }
+    @RequestMapping("kyselyt")
+    public String naytaKyselyt() {
+        return "kyselyt/kyselylista";
+    }
+}
+
---
+++ src/main/java/ohjelmisto/controller/KysymysController.java
@@ -0,0 +1,62 @@
+package ohjelmisto.controller;
+
+import ohjelmisto.ohjelmisto.dao.KysymysDAO;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.inject.Inject;
+
+/**
+ * Created by bferr on 2.5.2017.
+ */
+@Controller
+@RequestMapping(value="/secure")
+public class KysymysController {
+
+    @Inject
+    KysymysDAO dao;
+
+    public KysymysDAO getDao(){
+        return dao;
+    }
+
+    public void setDao(KysymysDAO dao){
+        this.dao = dao;
+    }
+
+    // Form luominen
+
+  /*  @RequestMapping (value="id", method= RequestMethod.GET)
+    public String getCreateForm(Model model){
+        Kysymys tyhjaKysymys = new KysymysImpl();
+        tyhjaKysymys.setOtsikko("oletusotsikko");
+        model.addAttribute("kysymys", tyhjaKysymys);
+        return "secure/add";
+    }
+
+    // Form tiedon vastaanotto
+    @RequestMapping (value="id", method= RequestMethod.GET)
+    public String create(@ModelAttribute(value="kysymys") KysymysImpl kysymys){
+        dao.lisaaKysymys(kysymys);
+        return "redirect:/kysymys/" + kysymys.getId();
+    }
+
+    // Tietojen näyttö
+    @RequestMapping (value="id", method= RequestMethod.GET)
+    public String getView(@PathVariable Integer id, Model model){
+        List<Kysymys> kysymys = dao.haeKaikki();
+        model.addAttribute("kysymys", kysymys);
+        return "secure/view";
+    }
+*/
+    @RequestMapping("kysymys.json")
+    public @ResponseBody List<Kysymys> kysymykset haeKaikki ();
+    return kysymykset;
+
+    @RequestMapping("kysymykset")
+    public String lisaaKysymys() {
+        return "kysymys/kysymyslista";
+
+    }
+}
