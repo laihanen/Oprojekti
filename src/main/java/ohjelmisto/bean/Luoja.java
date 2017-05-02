02356e07e658a0c5ce71173264837123f3822177
iikeli <iiro.alhonen@gmail.com>
2017-05-02 12:58:02 +0300

    Luojalle pohjaa

--- src/main/java/ohjelmisto/bean/Luoja.java
+++ src/main/java/ohjelmisto/bean/Luoja.java
@@ -4,4 +4,14 @@ package ohjelmisto.bean;
  * Created by bferr on 2.5.2017.
  */
 public interface Luoja {
+
+    public abstract int getId();
+
+    public abstract void setId(int id);
+
+    public abstract String getNimi();
+
+    public abstract void setNimi(String nimi);
+
+    
 }
