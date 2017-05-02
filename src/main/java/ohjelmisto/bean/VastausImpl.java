200327bb90c9c8528ef06381920cec1eb7c8a15e
laihanen <juho.laihanen@gmail.com>
2017-05-02 14:07:45 +0300

    hel

--- src/main/java/ohjelmisto/bean/VastausImpl.java
+++ src/main/java/ohjelmisto/bean/VastausImpl.java
@@ -4,4 +4,32 @@ package ohjelmisto.bean;
  * Created by bferr on 2.5.2017.
  */
 public class VastausImpl {
+
+    private int id;
+
+    private String avointeksti;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getAvointeksti() {
+        return avointeksti;
+    }
+
+    public void setAvointeksti(String avointeksti) {
+        this.avointeksti = avointeksti;
+    }
+
+    @Override
+    public String toString() {
+        return "VastausImpl{" +
+                "id=" + id +
+                ", avointeksti='" + avointeksti + '\'' +
+                '}';
+    }
 }
