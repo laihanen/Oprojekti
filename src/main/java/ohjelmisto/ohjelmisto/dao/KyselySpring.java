3284b8c335b0de4228eb56f3dbdb6b47832af29a
laihanen <juho.laihanen@gmail.com>
2017-05-03 14:33:53 +0300

    row

--- src/main/java/ohjelmisto/ohjelmisto/dao/KyselySpring.java
+++ src/main/java/ohjelmisto/ohjelmisto/dao/KyselySpring.java
@@ -1,7 +1,75 @@
 package ohjelmisto.ohjelmisto.dao;
 
+import ohjelmisto.bean.Kysely;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.PreparedStatementCreator;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
+import org.springframework.stereotype.Repository;
+
+import javax.inject.Inject;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.List;
+
 /**
  * Created by bferr on 3.5.2017.
  */
-public class KyselySpring {
+@Repository
+public class KyselySpring implements KyselyDAO {
+
+    @Inject
+    public JdbcTemplate jdbcTemplate;
+
+    public void setJdbcTemplate(JdbcTemplate jdbcTemplate){
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    public void lisaaKysely(Kysely k){
+
+        final String sql = "insert into kysely(nimi, luojanimi) values(?)";
+        final String nimi = k.getNimi();
+        final String luojanimi = k.getLuojaNimi();
+
+        KeyHolder idHolder = new GeneratedKeyHolder();
+
+        jdbcTemplate.update(new PreparedStatementCreator() {
+            public PreparedStatement createPreparedStatement(Connection con) throws SQLException {
+                PreparedStatement pre = con.prepareStatement(sql, new String[]{"id"});
+                pre.setString(1, nimi);
+                pre.setString(1, luojanimi);
+                return pre;
+            }
+        }, idHolder);
+        k.setId(idHolder.getKey().intValue());
+    }
+
+    public void poista(int id){
+
+        String sql = "delete from kysely where id = ?";
+        jdbcTemplate.update(sql, id);
+        System.out.println("Deleted Record with ID = " + id );
+        return;
+    }
+
+
+    public List<Kysely> haeOmat(String luojaNimi){
+        String sql = "select * from kysely where luojanimi = ?";
+        Object[] parametrit = new Object[] {luojaNimi};
+        RowMapper<Kysely> mapper = new KyselyRowMapper();
+        List<Kysely> kyselyt = jdbcTemplate.query(sql, mapper);
+
+        return kyselyt;
+    }
+
+
+    public List<Kysely> haeKaikki(){
+        String sql = "select * from kysely";
+        RowMapper<Kysely> mapper = new KyselyRowMapper();
+        List<Kysely> kyselyt = jdbcTemplate.query(sql, mapper);
+
+        return kyselyt;
+    }
 }
