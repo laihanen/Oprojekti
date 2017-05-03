5027969816ba442ae8a5fe6f6729bfa4700e2f75
idaliinaleksandra <idaliinalaine@gmail.com>
2017-05-03 12:42:48 +0300

    DAO

--- src/main/java/ohjelmisto/ohjelmisto/dao/KysymysDAO.java
+++ src/main/java/ohjelmisto/ohjelmisto/dao/KysymysDAO.java
@@ -1,7 +1,17 @@
 package ohjelmisto.ohjelmisto.dao;
 
+import ohjelmisto.bean.Kysymys;
+import java.util.List;
+
 /**
  * Created by bferr on 2.5.2017.
  */
 public interface KysymysDAO {
+
+    public abstract void lisaaKysymys(Kysymys kysymys);
+
+    public abstract void poista(int id);
+
+    public abstract List<Kysymys> haekaikki();
+
 }
