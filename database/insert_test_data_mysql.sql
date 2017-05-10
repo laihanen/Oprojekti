7371d39cfffcf69f579ceba4e1aee1de13f355d6
idaliinaleksandra <idaliinalaine@gmail.com>
2017-05-10 14:15:11 +0300

    kysely tietokannassa

--- database/insert_test_data_mysql.sql
+++ database/insert_test_data_mysql.sql
@@ -2,15 +2,25 @@ INSERT INTO luoja (nimi, sposti, salasana)
 VALUES
 ('Jorma Testaaja', 'jormatestaaja@joku.fi', 'salasana1234');
 
+INSERT INTO luoja (nimi, spoasti, salasana)
+VALUES
+('Liisa Testaaja', 'liisatestaaja@testi.fi', 'salasana1234')
+
 INSERT INTO kysely (nimi, luoja_id)
 VALUES
 ('Kyselyn lähtökohtana on tutkia Haaga-Helia ammattikorkeakoulun opiskelijoiden kouluviihtyvyyttä ja sen kehittämistä', 1);
-
 
 INSERT INTO otsikko (nimi)
 VALUES
 ('Perustiedot'),
 ('Viihtyvyys');
+
+INSERT INTO otsikko (nimi)
+VALUES
+('Koulusi olosuhteet'),
+('Sosiaaliset suhteet'),
+('Terveydentila'),
+('Kouluruokailu');
 
 INSERT INTO kysymystyyppi (nimi)
 VALUES
@@ -25,7 +35,17 @@ VALUES
 ('Siviilisääty', 1, 1, 1),
 ('Minulla on lapsia..', 1, 1, 1),
 ('Opiskelen lukukaudella..', 1, 1, 1),
-('Kerro mitkä kaikki asiat vaikuttavat koulussa viihtymiseen', 1, 2, 3);
+('Kerro mitkä kaikki asiat vaikuttavat koulussa viihtymiseen', 1, 2, 3),
+('Kuinka hyvin pystyn toteuttaman itsenäni koulussa', 1, 2, 4),
+('Vastaa alla oleviin opiskeluasi ammattikorkeakoulussa koskeviin väittämiin vain sinua parhiaten kuvaamalla vaihtoehdon numerolla', 1, 2, 4),
+('Oletko osallistunut viimeisen lukukauden aikana oppolaitoksesi järjestöjen timintaan', 1, 2, 1),
+('Kerro muutamalla sanalla, mitä mieltä olet jaksotetusta opiskelusta (4 jaksoa vuodessa)', 1, 2, 3),
+('Valitse kampus, jolla opiskelet', 1, 3, 3),
+('Merkitse väittämät, jotka pitävät paikkansa', 1, 3, 2),
+('Merkitse väittämät, jotka pitävät paikkansa', 1, 4, 2),
+('Mitä mieltä olet terveydentilastasi(Fyysinen)', 1, 5, 1),
+('Mitä mieltä olet terveydentilastasi (Henkinen)', 1, 5, 1),
+('Vastaa sinua kuvaavalla vaihtoehdolla', 1, 5, 1);
 
 INSERT INTO vaihtoehto(nimi, kysymys_id)
 VALUES
@@ -36,7 +56,12 @@ VALUES
 ('20-25', 2),
 ('25-30', 2),
 ('30-50', 2),
-('50+', 2);
+('50+', 2),
+('Sinkku', 3),
+('Parisuhteessa/naimisissa', 3),
+('En halua vastata', 3),
+('Kyllä', 4),
+('Ei', 4);
 
 INSERT INTO vastaus(avointekstivastaus, kysymys_id)
 VALUES
