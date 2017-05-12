3770a46c9ca4bb7b78fc85d571b3edfd3ca7983e
iikeli <iiro.alhonen@gmail.com>
2017-05-12 12:12:16 +0300

    Otsikoille pohjaa

--- src/main/java/ohjelmisto/dao/OtsikkoSpring.java
+++ src/main/java/ohjelmisto/dao/OtsikkoSpring.java
@@ -1,7 +1,26 @@
 package ohjelmisto.dao;
 
+import ohjelmisto.bean.Otsikko;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import javax.inject.Inject;
+
 /**
  * Created by bferr on 3.5.2017.
  */
-public class OtsikkoSpring {
+@Repository
+public class OtsikkoSpring implements OtsikkoDAO {
+
+    @Inject
+    public JdbcTemplate jdbcTemplate;
+
+    public void setJdbcTemplate(JdbcTemplate jdbcTemplate){
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    public void lisaaOtsikko(Otsikko otsikko) {
+
+        final String sql = "insert into"
+    }
 }
