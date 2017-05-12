b537fa3d385d736f0fef953da545d3b8e06741ef
hokoe <jan.hoglund@haaga-helia.fi>
2017-05-12 12:41:02 +0300

    LISSSÄÄÄ VASTAUS JUTTUJA!!

--- src/main/java/ohjelmisto/dao/VastausDAO.java
+++ src/main/java/ohjelmisto/dao/VastausDAO.java
@@ -1,9 +1,18 @@
 package ohjelmisto.dao;
 
+import ohjelmisto.bean.Vastaus;
+import java.util.List;
+
 /**
  * Created by bferr on 2.5.2017.
  */
 public interface VastausDAO {
 
+
+        public abstract void lisaaVastaus(Vastaus vastaus);
+
+        public abstract void poista(int id);
+
+        public abstract List<Vastaus> haeKaikki();
 
 }
