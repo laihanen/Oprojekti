4405f8668e5e98c2d81775be55dc5475a1da585a
hokoe <jan.hoglund@haaga-helia.fi>
2017-05-12 12:47:14 +0300

    LISSSÄÄÄ VASTAUS JUTTUJA!!

--- src/main/java/ohjelmisto/dao/VastausSpring.java
+++ src/main/java/ohjelmisto/dao/VastausSpring.java
@@ -6,6 +6,7 @@ import org.springframework.jdbc.core.PreparedStatementCreator;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.support.GeneratedKeyHolder;
 import org.springframework.jdbc.support.KeyHolder;
+import org.springframework.stereotype.Repository;
 
 import javax.inject.Inject;
 import java.sql.Connection;
@@ -13,7 +14,7 @@ import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.util.List;
 
-
+@Repository
 public class VastausSpring implements VastausDAO {
 
     @Inject
