88d03f1a3efcaf24b202d24e3d6488137457ebc6
iikeli <iiro.alhonen@gmail.com>
2017-05-17 16:50:54 +0300

    Varmaan väärin, but it's something

--- src/main/java/ohjelmisto/bean/VaihtoehtoVastaus.java
+++ src/main/java/ohjelmisto/bean/VaihtoehtoVastaus.java
@@ -4,4 +4,12 @@ package ohjelmisto.bean;
  * Created by Iiro on 17.5.2017.
  */
 public interface VaihtoehtoVastaus {
+
+    public abstract int getVaihtoehto_id();
+
+    public abstract void setVaihtoehto_id(int vaihtoehto_id);
+
+    public abstract int getVastaus_id();
+
+    public abstract void setVastaus_id(int vastaus_id);
 }
--- src/main/java/ohjelmisto/bean/VaihtoehtoVastausImpl.java
+++ src/main/java/ohjelmisto/bean/VaihtoehtoVastausImpl.java
@@ -1,7 +1,23 @@
 package ohjelmisto.bean;
 
+import javax.validation.constraints.Size;
+
 /**
  * Created by Iiro on 17.5.2017.
  */
-public class VaihtoehtoVastausImpl {
+public class VaihtoehtoVastausImpl implements VaihtoehtoVastaus {
+
+    private int vaihtoehto_id;
+
+    private int vastaus_id;
+
+    public int getVaihtoehto_id(){return vaihtoehto_id;}
+
+    public void setVaihtoehto_id(int vaihtoehto_id){this.vaihtoehto_id = vaihtoehto_id;}
+
+    public int getVastaus_id(){return vastaus_id;}
+
+    public void setVastaus_id(int vastaus_id){this.vastaus_id = vastaus_id;}
+
+
 }
